732f7e8665b840ddbb968465f9c4cececcce5bd5
Emanuel Targino <emanueltargino1@gmail.com>
2021-06-11 15:25:42 -0300

    resposta intervalos

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -16,6 +16,38 @@ using std::setprecision;
 int main(void)
 {
     // TODO: Adicione aqui a sua solução.
+    int x;
+
+    int totalNumbers = 0;
+    double intervals[5] = {0,0,0,0,0};
+
+    while( cin >> std::ws >> x) {
+
+        totalNumbers++;
+
+        if((0 <= x) && (x < 25)){
+            intervals[0]++;
+        }
+        else if((25 <= x) && (x < 50)){
+            intervals[1]++;
+        }
+        else if((50 <= x) && (x < 75)){
+            intervals[2]++;
+        }
+        else if((75 <= x) && (x < 100)){
+            intervals[3]++;
+        }
+        else{
+            intervals[4]++;
+        }
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        double result = (intervals[i]*100)/totalNumbers;  
+        cout << std::setprecision(4) << result << "\n";
+    }
+
 
     return 0;
 }
