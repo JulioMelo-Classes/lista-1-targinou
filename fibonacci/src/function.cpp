b7ad793e83f2f065e9df0e61ba1425cecf353e27
Emanuel Targino <emanueltargino1@gmail.com>
2021-06-11 14:53:09 -0300

    resposta fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,41 @@
 #include "function.h"
 
+
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
+    std::vector<unsigned int> sequence;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    if(n > 1){
+        
+        for (int i = 0; i < 2; i++)
+        {
+            sequence.push_back(1);
+        }
+        
+        int lastValue = 1;
+        int penultValue = 1;
+        int aux = 0;
+
+        while (true)
+        {
+            
+            if (lastValue > n)
+            {  
+                sequence.erase(sequence.end()-1);
+                return sequence;
+            }
+            else if (lastValue == n)
+            {
+                sequence.erase(sequence.end()-1);
+                return sequence;
+            }
+            else{
+                sequence.push_back(lastValue + penultValue);
+                aux = lastValue;
+                lastValue = lastValue + penultValue;
+                penultValue = aux;
+            }
+            
+        }
+    }
 }
